87255607c4293bc485001e71729f16bce9d4e520
yyxuzq <yyxu04273@gmail.com>
2026-07-03 16:16:53 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,33 @@
+const CACHE_NAME = 'ours-planet-v1';
+const ASSETS = [
+  './',
+  './ours.html',
+  './manifest.webmanifest',
+  './daytime.jpg',
+  './night-sea.jpg',
+  './sky.jpg'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(cached => {
+      return cached || fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      );
+    })
+  );
+});
